43efebf853adc4cffd194756e7fcf003ffd769bf
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-12-06 21:13:23 +0000

    define api endpoints

---
+++ lib/core/data/network/config.dart
@@ -0,0 +1,3 @@
+const String activeSymbols = "https://api.deriv.com/api-explorer/#active_symbols";
+const String ticks = "https://api.deriv.com/api-explorer/#ticks";
+const String forget = "https://api.deriv.com/api-explorer/#ticks";
