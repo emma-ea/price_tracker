b18124a5286ce2c6757684b2d2310edfd8cbb1ec
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-12-08 00:05:30 +0000

    adjust drop down width

--- lib/price_tracker/presentation/widgets/list_drop_down.dart
+++ lib/price_tracker/presentation/widgets/list_drop_down.dart
@@ -3,31 +3,27 @@ import 'package:price_tracker/price_tracker/data/models/market_symbols.dart';
 
 class ListDropDown extends StatelessWidget {
 
-  final List<ActiveSymbol> items;
   final ValueChanged<String?> onChanged;
-  String hint;
+  final List<DropdownMenuItem<String>> items;
+  Widget hint;
 
   ListDropDown({
     super.key,
     required this.items,
     required this.onChanged,
-    this.hint = "Markets"
+    this.hint = const Text("Markets"),
   });
 
   @override
   Widget build(BuildContext context) {
-    return  DropdownButton<String>(
-      hint: Text(hint),
-      items: items.map<DropdownMenuItem<String>>((ActiveSymbol item) {
-        return DropdownMenuItem(
-          value: item.symbol,
-          child: Text(item.displayName!),
-          onTap: () {
-            hint = item.displayName!;
-          },
-        );
-      }).toList(), 
-      onChanged: onChanged
+    return  SizedBox(
+      width: MediaQuery.of(context).size.width * .7,
+      child: DropdownButton<String>(
+        isExpanded: true,
+        hint: hint,
+        items: items,
+        onChanged: onChanged
+      ),
     );
   }
 }
