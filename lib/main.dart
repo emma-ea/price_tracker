5722c720858e4f0f4ee9fac1d0ae70a8a446e31f
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-12-15 18:08:50 +0000

    move bloc providers up the tree

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:price_tracker/core/di/configure_injectors.dart';
+import 'package:price_tracker/core/di/constants.dart';
 import 'package:price_tracker/core/failures.dart';
+import 'package:price_tracker/dark_mode/presentation/state/dark_mode_cubit.dart';
 import 'package:price_tracker/price_tracker/domain/di/price_tracker_module_injector.dart';
 import 'package:price_tracker/price_tracker/domain/usecases/available_market_symbol.dart';
 import 'package:price_tracker/price_tracker/domain/usecases/available_symbol_ticks.dart';
@@ -19,7 +21,23 @@ Future<void> main() async {
 
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => DarkModeCubit()..darkMode()
+        ),
+        BlocProvider( 
+          create: (context) => PriceTrackerCubit(
+            PriceTrackerModuleInjector.resolve<AvailableSymbols>(),
+            PriceTrackerModuleInjector.resolve<AvailableTicks>(),
+            PriceTrackerModuleInjector.resolve<DisposeConnection>()
+          )..getMarketSymbols(),
+        ),
+      ], 
+      child: const MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -27,45 +45,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => PriceTrackerCubit(
-        PriceTrackerModuleInjector.resolve<AvailableSymbols>(),
-        PriceTrackerModuleInjector.resolve<AvailableTicks>(),
-        PriceTrackerModuleInjector.resolve<DisposeConnection>()
-      )..getMarketSymbols(),
-      child: MaterialApp(
-        title: 'Price Tracker',
-        theme: ThemeData.light(),
-        home: BlocListener<PriceTrackerCubit, PriceTrackerState>(
-          listener: ((context, state) {
-            state.maybeWhen(
-              orElse: (){},
-              symbolsLoaded: ((payload) {
-                Navigator.of(context)
-                  .pushReplacement(MaterialPageRoute(builder: (_) => const PriceTracker()));
-              }),
-              error: (payload) {
-                infoDialog(
-                  context: context, 
-                  title: "Error", 
-                  msg: payload.error,
-                  cancel: () => Navigator.pop(context),
-                  retry: () {
-                    Navigator.pop(context);
-                    context.read<PriceTrackerCubit>().getMarketSymbols();
-                  }
-                );
-              },
-            );
-          }),
-          child: Builder(
-            builder: (ctx) => ctx.watch<PriceTrackerCubit>().state.maybeWhen(
-                error: (payload) => ExceptionScreen(NetworkFailure(""), error: payload.error,),
-                loading: (payload) => const LoadingIndicator(),
-                orElse: () => const LoadingIndicator(),
-              )
-          ),
+    return MaterialApp(
+      title: TITLE,
+      theme: ThemeData( 
+        brightness: BlocProvider.of<DarkModeCubit>(context).state.payload.darkMode?.brightness
       ),
-    ));
+      home: BlocListener<PriceTrackerCubit, PriceTrackerState>(
+        listener: ((context, state) {
+          state.maybeWhen(
+            orElse: (){},
+            symbolsLoaded: ((payload) {
+              Navigator.of(context)
+                .pushReplacement(MaterialPageRoute(builder: (_) => const PriceTracker()));
+            }),
+            error: (payload) {
+              infoDialog(
+                context: context, 
+                title: "Error", 
+                msg: payload.error,
+                cancel: () => Navigator.pop(context),
+                retry: () {
+                  Navigator.pop(context);
+                  context.read<PriceTrackerCubit>().getMarketSymbols();
+                }
+              );
+            },
+          );
+        }),
+        child: Builder(
+          builder: (ctx) => ctx.watch<PriceTrackerCubit>().state.maybeWhen(
+              error: (payload) => ExceptionScreen(NetworkFailure(""), error: payload.error,),
+              loading: (payload) => const LoadingIndicator(),
+              orElse: () => const LoadingIndicator(),
+            )
+        ),
+    ),
+    );
   }
 }
